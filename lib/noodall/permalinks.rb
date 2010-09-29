f119646a343bee4e4b5b1c6cf1b8c3d15cd183bd
Daniel Craig <daniel@wearebeef.co.uk>
2010-09-29 17:01:21 +0100

    change to noodall module

--- lib/noodall/permalinks.rb
+++ lib/noodall/permalinks.rb
@@ -1,10 +1,12 @@
-module Noodall::Permalinks
-  # Handy place for all the permalink methods
-  def node_path(node, options = {})
-    node_permalink_path node.permalink, options
-  end
-
-  def node_url(node, options = {})
-    node_path(node, options.merge(:only_path => false))
+module Noodall
+  module Permalinks
+    # Handy place for all the permalink methods
+    def node_path(node, options = {})
+      node_permalink_path node.permalink, options
+    end
+  
+    def node_url(node, options = {})
+      node_path(node, options.merge(:only_path => false))
+    end
   end
 end
