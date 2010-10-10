0423a3deabbc16ee69845ed4118eab34aec6f8c8
Steve England <steve@wearebeef.co.uk>
2010-10-10 23:48:47 +0100

    Heroku overwrites database.yml so use initialiser to set MongoHQ URI

--- demo/initializers/mongo_mapper.rb
+++ demo/initializers/mongo_mapper.rb
@@ -1,5 +1,13 @@
+if ENV['MONGOHQ_URL']
+  config = {Rails.env => {'uri' => ENV['MONGOHQ_URL']}}
+else
+  config = Rails.configuration.database_configuration
+end
+
 MongoMapper.setup(
-  Rails.configuration.database_configuration,
+  config,
   Rails.env,
-  { :logger    => Rails.logger, :passenger => true }
+  { :logger    => Rails.logger }
 )
+
+
