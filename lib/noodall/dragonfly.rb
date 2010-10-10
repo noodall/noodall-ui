f045f47145d0dc963bf48dd79fce22529d991251
Steve England <steve@wearebeef.co.uk>
2010-10-11 00:21:16 +0100

    Use mongomappers db for dragonfly

--- lib/noodall/dragonfly.rb
+++ lib/noodall/dragonfly.rb
@@ -1,13 +1,21 @@
 require 'dragonfly'
 require 'noodall/video_thumbnail_encoder'
 
+module Noodall
+  class DragonflyDataStorage < Dragonfly::DataStorage::MongoDataStore
+    def db
+      MongoMapper.database
+    end
+  end
+end
+
 # Configuration for processing and encoding
 app = Dragonfly::App[:noodall_assets]
 app.configure_with(:rmagick)
 app.configure_with(:rails)
 app.encoder.register(VideoThumbnailEncoder)
 app.configure do |c|
-  c.datastore = Dragonfly::DataStorage::MongoDataStore.new Rails.configuration.database_configuration
+  c.datastore = Noodall::DragonflyDataStorage.new
   c.secret = '2406b0d72aaf812659babdf53eb49d4812c2cc59' unless Rails.env.test?
 end
 app.url_suffix = proc{|job|
