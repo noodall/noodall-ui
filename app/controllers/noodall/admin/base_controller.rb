73e7c0b4da79b48bd5f05995f3cef90b0a23b440
Steve England <steve@wearebeef.co.uk>
2010-11-05 12:21:07 +0000

    Adding Transgression catcher

--- app/controllers/noodall/admin/base_controller.rb
+++ app/controllers/noodall/admin/base_controller.rb
@@ -3,6 +3,14 @@ module Noodall
     class BaseController < ApplicationController
       layout 'noodall_admin'
       before_filter :authenticate_user!
+
+      rescue_from Canable::Transgression, :with => :permission_denied
+
+      private
+        def permission_denied
+          flash[:error] = "You do not have permission to do that"
+          redirect_to :back
+        end
     end
   end
 end
