eaa8a2c634bdd1af0959af5df788dea335190e1b
Daniel Craig <daniel@wearebeef.co.uk>
2010-12-03 15:33:00 +0000

    remove root generators folder

--- generators/component/templates/factory.rb
+++
@@ -1,5 +0,0 @@
-Factory.define :<%= singular_name %>, :parent => :node, :class => <%= singular_name.titleize %> do |<%= singular_name %>|
-<% for attribute in attributes -%>
-  <%= singular_name %>.<%= attribute.name %> {Faker::Lorem.words(5).join(' ').titleize}
-<% end -%>
-end
--- generators/component/templates/model.rb
+++
@@ -1,8 +0,0 @@
-class <%= singular_name.camelize %> < Noodall::Node
-# sub_templates <%= singular_name.camelize %>
-# root_template!
-
-# small_slots 4
-
-  
-end
