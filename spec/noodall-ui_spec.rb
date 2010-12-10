072207b5d335581e284edc111985f16501358a8e
Jamie Dyer <jamie@kernowsoul.com>
2010-12-10 14:15:53 +0000

    removed unused spec files

--- spec/noodall-ui_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Noodall::UI do
-  it "should return the defaut admin menu" do
-    Noodall::UI.menu_items.should have(2).things
-
-    Noodall::UI.menu_items['Contents'].should == :noodall_admin_nodes_path
-  end
-end
--- spec/spec_helper.rb
+++
@@ -1,4 +0,0 @@
-require 'noodall-ui'
-
-Rspec.configure do |config|
-end
