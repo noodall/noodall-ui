f151f65f3c7672fd83719dcc38f41498f5f0a03d
stengland <steve@wearebeef.co.uk>
2011-01-21 16:49:22 +0000

    Allowing string permalinks to be passed to node_path

--- lib/noodall/permalinks.rb
+++ lib/noodall/permalinks.rb
@@ -2,9 +2,10 @@ module Noodall
   module Permalinks
     # Handy place for all the permalink methods
     def node_path(node, options = {})
-      node_permalink_path node.permalink.to_s, options
+      node = node.is_a?(String) ? node : node.permalink.to_s
+      node_permalink_path node, options
     end
-  
+
     def node_url(node, options = {})
       node_path(node, options.merge(:only_path => false))
     end
