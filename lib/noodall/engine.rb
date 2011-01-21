65a5219da89e7479c60fe40ca0285a72cf692341
stengland <steve@wearebeef.co.uk>
2011-01-21 15:22:29 +0000

    Adding sitemap loading

--- lib/noodall/engine.rb
+++ lib/noodall/engine.rb
@@ -1,11 +1,20 @@
 require 'dynamic_form'
-require "will_paginate"
+require 'will_paginate'
+require 'noodall/site'
 
 module Noodall
   class Engine < Rails::Engine
 
     initializer "static assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
+    end
+
+    initializer "load site map" do |app|
+      begin
+        Noodall::Site.map = YAML::load(File.open(File.join(Rails.root, 'config', 'sitemap.yml')))
+      rescue Exception => e
+        puts "Failed to load noodall site map: #{e}"
+      end
     end
 
   end
