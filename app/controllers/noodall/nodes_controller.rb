84ce400f59f48dea403b8392b0dc14d0eb1d761b
stengland <steve@wearebeef.co.uk>
2011-02-03 13:39:13 +0000

    Remove last modified on 404 as well

--- app/controllers/noodall/nodes_controller.rb
+++ app/controllers/noodall/nodes_controller.rb
@@ -53,6 +53,7 @@ module Noodall
       end
       # Expire any caching already set
       expires_now
+      headers.delete('Last-Modified')
 
       render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false, :content_type => "text/html"
     end
--- lib/noodall/ui/version.rb
+++ lib/noodall/ui/version.rb
@@ -1,5 +1,5 @@
 module Noodall
   module UI
-    VERSION = "0.1.13"
+    VERSION = "0.1.14"
   end
 end
