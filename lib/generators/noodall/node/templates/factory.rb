8d94c101126b3001ed613ed4602b8fcd0c9ec93a
stengland <steve@wearebeef.co.uk>
2011-03-30 16:37:37 +0100

    Adding extra info to generated files

--- lib/generators/noodall/node/templates/factory.rb
+++ lib/generators/noodall/node/templates/factory.rb
@@ -2,6 +2,7 @@ Factory.define :<%= file_name %> do |<%= file_name %>|
   <%= file_name %>.title { Faker::Lorem.words(3).join(' ') }
   <%= file_name %>.body { Faker::Lorem.paragraphs(6) }
   <%= file_name %>.published_at { Time.now }
+  <%= file_name %>.publish true
 <% for attribute in attributes -%>
   <%= file_name %>.<%= attribute.name %> {Faker::Lorem.words(5).join(' ').titleize}
 <% end -%>
