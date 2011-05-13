d722090db5a463bd8572840e8ec86d66437f43dc
Alex Heaton <alex@heaton.me>
2011-05-13 12:10:11 +0100

    modify the additional_classes on components, to work with ruby 1.9 too

--- app/helpers/noodall/components_helper.rb
+++ app/helpers/noodall/components_helper.rb
@@ -11,21 +11,23 @@ module Noodall::ComponentsHelper
       index = args.index(slot_code)
       component = components[index]
 
-      additional_classes = []
-      additional_classes << slot_code.split("_").shift unless slot_code.split("_").shift.nil?
-      additional_classes << 'penultimate' if slot_code == args[args.size - 2]
-      additional_classes << 'last' if args.last == slot_code
-      additional_classes << 'first' if args.first == slot_code
+      #Build a string here to stop ruby 1.9 outputting a normal printed array
+      additional_classes = ""
+      additional_classes << ' ' + slot_code.split("_").shift + ' ' unless slot_code.split("_").shift.nil?
+      additional_classes << ' penultimate ' if slot_code == args[args.size - 2]
+      additional_classes << ' last ' if args.last == slot_code
+      additional_classes << ' first ' if args.first == slot_code
 
       # pass a flag to the view to add an expanded html class
-      component(node, slot_code, ( components[index + 1].nil? && index < (args.size - 1) ), additional_classes.join(' ')).to_s
+      component(node, slot_code, ( components[index + 1].nil? && index < (args.size - 1) ), additional_classes).to_s
     end.join.html_safe
   end
 
   def component(node, slot_code, expand = false, additional_classes = '')
     component = node.send(slot_code)
 
-    additional_classes = []
+    #Use a string here to stop ruby 1.9 outputting a normal printed array
+    additional_classes = ""
     additional_classes << slot_code.split("_").shift unless slot_code.split("_").shift.nil?
 
     render :partial => "components/#{component.class.name.underscore}", :locals => { :slot_code => slot_code, :component => component, :expand => expand, :additional_classes => additional_classes } unless component.nil?
