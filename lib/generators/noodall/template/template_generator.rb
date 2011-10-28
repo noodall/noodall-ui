997c9690db9c71b1d88a4428d7f90d43cada1b0f
Steve England <steve@wearebeef.co.uk>
2011-10-28 15:54:34 +0100

    Renaming the node generator to make moore sense

--- lib/generators/noodall/template/template_generator.rb
+++ lib/generators/noodall/template/template_generator.rb
@@ -1,5 +1,5 @@
 module Noodall
-  class NodeGenerator < Rails::Generators::NamedBase
+  class TemplateGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('../templates', __FILE__)
     argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
 
